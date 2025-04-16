9d0e8e5f7767f8e6f1d66a3b696d5e1429d0d35b
Sibiryaq <4totakoeaaa@mail.ru>
2025-04-16 18:25:21 +0700

    1st commit

---
+++ src/test/java/org/tolstikov/starter/StarterApplicationTests.java
@@ -0,0 +1,13 @@
+package org.tolstikov.starter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
